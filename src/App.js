9c062ff057908667a84edbd97d0527aa5c16e923
Yeo Zhi Long <44795294+yzhilong@users.noreply.github.com>
2021-07-23 22:53:26 +0800

    Initial implementation of flask

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react"
+import React, { Component, useEffect, useState } from "react"
 import { Route, Switch } from 'react-router-dom'
 import Favourites from "./pages/Favourites"
 import NavBar from "./components/NavBar"
@@ -12,10 +12,34 @@ function App() {
     // favourites should be a set of strings, where each element is "uni_name, MOD1, MOD2, ..., MODn"
     // modules must be in sorted order
     const [ favourites, setFavourites ] = useState(new Set())
+
+
+    // THIS IS DIAGNOSTIC ONLY! REMOVE LATER
+    const [result, setResult] = useState({});
+    const body = {
+        essential_modules: ['CS2040','CS2030'],
+        optional_modules: [],
+        schools: [],
+        countries: [],
+        continents: []
+      }
+    useEffect(() => {
+      fetch('http://127.0.0.1:5000/selector', {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(body)
+      }).then(res => res.json()).then(data => {
+        console.log(data);
+        setResult(data);
+      }).catch(err => console.log(err))
+    }, [])
     
 
     return (
         <div>
+            {JSON.stringify(result)}
             <NavBar />
             <Route path="/" exact>
                 <Selector 
@@ -32,19 +56,21 @@ function App() {
                     setFavourites={setFavourites}
                 />
             </Route>
-            {/* {page === "Selector" 
-                ? <Selector 
-                    favourites={favourites}
-                    setFavourites={setFavourites}
-                    /> 
-                : page === "Favourites"
-                ? <Favourites
-                    favourites={favourites}
-                    setFavourites={setFavourites}/>
-                : page === "Department"
-                ? null
-                : null
-            } */}
+            {
+            // {page === "Selector" 
+            //     ? <Selector 
+            //         favourites={favourites}
+            //         setFavourites={setFavourites}
+            //         /> 
+            //     : page === "Favourites"
+            //     ? <Favourites
+            //         favourites={favourites}
+            //         setFavourites={setFavourites}/>
+            //     : page === "Department"
+            //     ? null
+            //     : null
+            // } 
+            }
         </div>
     )
 }
